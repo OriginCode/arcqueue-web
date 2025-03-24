a584182a7744c4d5590fc41c1654291a7b3a2756
Kaiyang Wu <self@origincode.me>
2025-03-23 22:29:08 -0700

    chore: init
    Signed-off-by: Kaiyang Wu <self@origincode.me>

---
+++ index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {BrowserRouter} from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+
+import App from './App';
+
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
+    </React.StrictMode>
+);
